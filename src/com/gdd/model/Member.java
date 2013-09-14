6f5a6ecf8866762dc12821640d0691da1350a90e
gongdongdong <gdd330282@aliyun.com>
2013-09-14 16:52:58 +0800

    服务端的功能完工了。

--- src/com/gdd/model/Member.java
+++ src/com/gdd/model/Member.java
@@ -6,30 +6,36 @@ public class Member {
 	private String username = null;
 	private String password = null;
 	private String workcode = null;
-	
-	public void setId(String id){
+
+	public void setId(String id) {
 		this.id = id;
 	}
-	public void setUsername(String username){
+
+	public void setUsername(String username) {
 		this.username = username;
 	}
-	public void setPassword(String password){
+
+	public void setPassword(String password) {
 		this.password = password;
 	}
-	public void setWorkcode(String workcode){
+
+	public void setWorkcode(String workcode) {
 		this.workcode = workcode;
 	}
-	
-	public String getId(){
+
+	public String getId() {
 		return this.id;
 	}
-	public String getUsername(){
+
+	public String getUsername() {
 		return this.username;
 	}
-	public String getPassword(){
+
+	public String getPassword() {
 		return this.password;
 	}
-	public String getWorkcode(){
+
+	public String getWorkcode() {
 		return this.workcode;
 	}
 }
--- src/com/gdd/utils/SignBusiness.java
+++ src/com/gdd/utils/SignBusiness.java
@@ -1,12 +1,43 @@
 package com.gdd.utils;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import com.gdd.db.MyDatabaseConnection;
+import com.gdd.model.Signtime;
+
 public class SignBusiness {
+	// 数据相关的
+	private Signtime signtime = null;
+
+	private int curDay = 0;
+	private int curHour = 0;
+	private int curMinute = 0;
+	// 工具相关的
+	private MyDatabaseConnection busiConnection = null;
 
 	public SignBusiness(String username) {
-		
+		// 初始化数据和工具
+		signtime = new Signtime();
+		busiConnection = new MyDatabaseConnection();
+		// 获取时间相关的数据
+		Date date = new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		curDay = cal.get(Calendar.DAY_OF_MONTH);
+		curHour = cal.get(Calendar.HOUR_OF_DAY);
+		curMinute = cal.get(Calendar.MINUTE);
+		// 设置时间相关的数据
+		signtime.setCome_tiem(String.valueOf(curHour) + ":"
+				+ String.valueOf(curMinute));
+		signtime.setLeave_time(String.valueOf(curHour) + ":"
+				+ String.valueOf(curMinute));
+		signtime.setCurrentday(String.valueOf(curDay));
+		signtime.setUsername(username);
+
 	}
-	
-	public void updatememberinfo(){
-		
+
+	public void updatememberinfo() {
+		busiConnection.executesql(signtime, 2);
 	}
 }
